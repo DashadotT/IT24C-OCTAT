e40a78e78293a36c7dbe7eec1dd829aaf646bb49
DashadotT <markjovanoctat@gmailc.com>
2024-11-02 09:37:57 +0800

    L11-Initialize city input and button elements for weather retrieval

--- L11-OpenWeather/L11_Applet5.js
+++ L11-OpenWeather/L11_Applet5.js
@@ -2,5 +2,9 @@ class WeatherApp {
     constructor() {
         //API Key
         this.apiKey = document.getElementById('apiKeyInput');
+
+        //Text Inputapplet-5/applet-5.js
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
     }
 }
